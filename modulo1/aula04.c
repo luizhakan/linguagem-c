ca1dd1a18b336baeff58110bb3c4fb808b33373a
luizhakan <uefibiosxx@gmail.com>
2022-03-27 22:27:47 -0300

    Modulo 1

---
+++ modulo1/aula04.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main(){ 
+    printf("O %s tem %d anos", "Luiz", 19); // tem que ser a mesma ordem de procedência
+    printf("\nSeu peso atual é de %.2fkg\n", 65.6);
+    printf("O seu sexo é %c", 'M');
+}
