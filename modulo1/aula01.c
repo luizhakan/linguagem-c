fe6f3575f59ed81e103c5da93a7f79ca5022125b
luizhakan <uefibiosxx@gmail.com>
2022-03-27 21:51:06 -0300

    Modulo 1

---
+++ modulo1/aula01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    printf("Olá, mundo!");
+    // me livrei da maldição :)
+    printf("\nSport\ncampeão\ndo\nNordeste\n");
+    printf("Sport\t3\tx\t1\tCRB\a\n");
+    printf("Estou 100%% feliz hoje :)\n");
+    printf("C é \"SUPER\" fácil!");
+}
