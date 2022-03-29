b5ee93a29a578b8a8b2a5ff7b6abf2a69f657dfa
luizhakan <uefibiosxx@gmail.com>
2022-03-29 16:51:01 -0300

    Modulo 1

---
+++ modulo1/aula05.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    unsigned int idade = 19;
+    char *nome = "Luiz";
+    char *sexo = "Masculino";
+    float peso = 65.0;
+
+    printf("%s é do sexo %s, tem %i anos e pesa %.2fKg", nome, sexo, idade, peso);
+}
