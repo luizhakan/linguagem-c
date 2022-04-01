ec1a7487b09fe5ecb423d2995c7b8e62798012fe
luizhakan <uefibiosxx@gmail.com>
2022-04-01 18:59:03 -0300

    Módulo 1

--- modulo1/aula07.c
+++ modulo1/aula07.c
@@ -4,8 +4,42 @@
 void main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int n;
-    printf("Digite um número inteiro : ");
-    scanf("%d", &n);
-    printf("\n %d", &n);
+    // lendo números
+    // float m;
+    // int n;
+
+    // printf("Digite um número real : ");
+    // scanf("%f", &m);
+    // printf("Digite um número inteiro : ");
+    // scanf("%d", &n);
+    // printf("Número real : %.2f \n", m);
+    // printf("Número inteiro : %d \n", n);
+
+    // lendo caracteres
+    // char r;
+    // char s;
+
+    // printf("Digite uma letra : ");
+    // fflush(stdin);
+    // scanf("%c", &r);
+
+    // printf("Digite outra letra : \n");
+    // fflush(stdin);
+    // scanf("%c", &s);
+
+    // printf("Primeira letra : %c \nSegunda letra : %c", r, s);
+
+    // lendo strings
+
+    char nome[20];
+    char endereco[60];
+    
+    printf("Digite seu nome : ");
+    fgets(nome, 20, stdin);
+
+    printf("Endereço : ");
+    fgets(endereco, 60, stdin);
+
+    printf("\nNome : %s", nome);
+    printf("Endereço : %s", endereco);
 }
