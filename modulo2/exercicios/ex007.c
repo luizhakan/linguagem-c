6b22c1268379af5e71bd3cbc646f01725ddce993
luizhakan <uefibiosxx@gmail.com>
2022-04-10 20:01:40 -0300

    Exercicios

---
+++ modulo2/exercicios/ex007.c
@@ -0,0 +1,16 @@
+// DOBRO E TERÇA PARTE
+#include <stdio.h>
+#include <locale.h>
+
+void main()
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("Digite um número : ");
+    float numero;
+    scanf("%f", &numero);
+    float dobro = numero * 2;
+    float tercaParte = numero * 3;
+
+    printf("Número %.2f, dobro é %.2f e a terça parte é %.2f", numero, dobro, tercaParte);
+}
