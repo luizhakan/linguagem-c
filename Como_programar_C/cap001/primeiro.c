1de575c1a39ce0d5d677a2f6e4dc9572aaf7c0ed
luizhakan <uefibiosxx@gmail.com>
2022-07-15 18:30:21 -0300

    Primeiro programa em C 15/07/2022 - Primeiro programa na linguagem C

---
+++ Como_programar_C/cap001/primeiro.c
@@ -0,0 +1,22 @@
+/*Primeiro programa em C*/
+
+main()
+{
+    printf("Bem vindo ao C\n");
+
+    // imprimindo várias linhas com um único printf
+    printf("Bem\nvindo\nao\nC\n");
+}
+
+/*
+\n - Nova linha
+\t - Move o cursos para a próxima marca de tabulação
+\a - Faz soar a campainha do sistema
+\\ - Caractere de barra invertida
+\"" - Aspas duplas
+%% - Simbolo de porcentagem
+
+Boa prática
+-   O último caracter impresso sempre deve ser o \n
+-   Identação é sempre importante
+*/
