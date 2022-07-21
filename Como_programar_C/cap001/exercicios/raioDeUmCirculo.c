a0c37c816987976db59948c23d296bdddffec5e1
luizhakan <luizhakan2@protonmail.com>
2022-07-20 21:36:23 -0300

    Calculando o raio de um círculo 20/07/2022 - usando a biblioteca math e cs50, eu consegui completar de maneira mais eficiente este exercício.

---
+++ Como_programar_C/cap001/exercicios/raioDeUmCirculo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include <cs50.h>
+#include <math.h>
+
+void circulo();
+void main()
+{
+    int valor = get_int("Área: ");
+    setlocale(LC_ALL, "Portuguese");
+    circulo(valor);
+}
+
+void circulo(int area){
+    int raio = 3.16 * pow(area, area);
+    printf("A área do círculo é : %d\n", raio);
+}
