4fc3b665bc16f12d749d8d2b4c7d4ad63e68f504
luizhakan <luizhakan2@protonmail.com>
2022-08-04 23:02:47 -0300

    repetição controlada por contador usando for

---
+++ Como_programar_C/cap004/repContadorFor.c
@@ -0,0 +1,7 @@
+// repetição controlada por contador com for
+#include <stdio.h>
+void main()
+{
+    for(int contador = 1; contador <= 10; contador++) /*inicialização, condição e incremento*/
+    printf("%d\n", contador); // instrução do laço
+}
