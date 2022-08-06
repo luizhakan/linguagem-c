52b25a9980ee33f1fcd3f2f1b2532e94c4fd1d68
luizhakan <luizhakan2@protonmail.com>
2022-08-05 21:52:18 -0300

    Fazendo calculo de juros compostos 05/08/2022

---
+++ Como_programar_C/cap004/jurosCompostos.c
@@ -0,0 +1,18 @@
+// fazendo calculo de juros compostos
+// fórmula : a = p(l + r)^n
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+void main()
+{
+setlocale(LC_ALL, "Portuguese");
+    int ano;
+    double quantia, principal = 1000.0, taxa = .05;
+    printf("%4s%21s\n", "Ano", "Saldo na conta");
+    
+    for(ano = 1; ano <= 10; ano++){
+    quantia = principal * pow(1 + taxa, ano);
+    printf("%4d%21.2f\n", ano, quantia);
+    }
+}
