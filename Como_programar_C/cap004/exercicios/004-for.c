5e52c64c195c048d75892028eb8874ece512ed55
luizhakan <luizhakan2@protonmail.com>
2026-01-26 10:32:13 -0300

    Remove multiple C source files and their corresponding executables from the Como_programar_C project, including exercises and examples related to credit limits, fuel consumption rates, infinite loops, average calculations, and various control structures. This cleanup helps streamline the project by eliminating unused or redundant code.

--- Como_programar_C/cap004/exercicios/004-for.c
+++
@@ -1,9 +0,0 @@
-/* escreva instruções for que imprimam os seguintes valores */
-#include <stdio.h>
-
-void main()
-{
-    int contador;
-    for(contador = 19; contador <= 51; contador += 8)
-    printf("%d\n", contador);
-}
